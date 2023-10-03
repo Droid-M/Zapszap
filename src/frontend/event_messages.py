8fc479153fa95db272814af8a23741039e77cb6e
Droid-M <marcosvferreira92@gmail.com>
2023-10-03 01:28:21 -0300

    update settings.json, config.env, event_messages.py and main.py

--- src/frontend/event_messages.py
+++ src/frontend/event_messages.py
@@ -1,3 +1,4 @@
+#SECTION - SOCKET SERVER
 def waiting_new_connection_message():
     print("Aguardando conexões...")
 
@@ -6,3 +7,15 @@ def new_client_connected_message(client_address):
 
 def socket_server_interrupted_message():
     print("Servidor interrompido pelo usuário.")
+#!SECTION
+
+#SECTION - SEARCH IP'S
+def on_start_ip_search_message():
+    print("Iniciando varredura de dispositivos na rede. Por favor, aguarde...")
+
+def on_present_device_in_network_message(ip):
+    print(f"{ip} - Presente na rede") 
+ 
+def on_absent_device_in_network_message(ip):
+    print(f"{ip} - Ausente na rede") 
+#!SECTION
--- src/main.py
+++ src/main.py
@@ -1,9 +1,25 @@
 from backend import server
-from frontend import main_menu
-from frontend import event_messages
+from frontend import main_menu, event_messages
+from helpers import socket, file
 import threading
+
+present_network_ips = []
+
+def on_present_device(ip):
+    present_network_ips.append(ip)
+    event_messages.on_present_device_in_network_message(ip)
+
+def on_absent_device(ip):
+    event_messages.on_absent_device_in_network_message(ip)
 
 if __name__ == '__main__':
     server_service = threading.Thread(target=server.start)
     server_service.start(event_messages.waiting_new_connection_message, event_messages.new_client_connected_message, event_messages.socket_server_interrupted_message)
+    event_messages.on_start_ip_search_message()
+    socket.scan_local_network_ips(
+        file.env('IP_RANGE', '192.168.0'),
+        on_present_device,
+        on_absent_device,
+        file.env('MAX_DEVICE_QTY', 255)
+    )
     main_menu.show()
