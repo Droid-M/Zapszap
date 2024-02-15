40590d6d243d8886a067c62a4711194932ca6039
Droid-M <62139366+Droid-M@users.noreply.github.com>
2024-02-15 13:11:24 -0300

    copy 1 file and update 1 file

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Função para confirmar a execução do comando de instalação de dependências
+confirmar_execucao() {
+    read -p "Deseja executar 'pip install -r requirements.txt'? (s/n): " resposta
+    if [[ "$resposta" =~ ^[Ss]$ ]]; then
+        pip install -r ~/Documentos/GitHub/Zapszap/src/requirements.txt
+    fi
+}
+
+confirmar_execucao
+x-terminal-emulator -e "./run.sh" &
+x-terminal-emulator -e "./auto_pull.sh" &
--- run_without_docker.sh
+++ run_without_docker.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-# Função para confirmar a execução do comando de instalação de dependências
-confirmar_execucao() {
-    read -p "Deseja executar 'pip install -r requirements.txt'? (s/n): " resposta
-    if [[ "$resposta" =~ ^[Ss]$ ]]; then
-        pip install -r ~/Documentos/GitHub/Zapszap/src/requirements.txt
-    fi
-}
-
-confirmar_execucao
-x-terminal-emulator -e "sudo python3 ./src/main.py" &
-x-terminal-emulator -e "./auto_pull.sh" &
+sudo python3 ./src/main.py
