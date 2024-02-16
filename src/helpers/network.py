9c660083461e16b923968857c0538ae7aa166df0
Droid-M <62139366+Droid-M@users.noreply.github.com>
2024-02-16 16:00:45 -0300

    update network.py

--- src/helpers/network.py
+++ src/helpers/network.py
@@ -21,9 +21,9 @@ def check_network():
         if is_online():
             file.log("network.log", "is Online")
             if stayed_offline:
+                print("\n\nVocê está online novamente\n\n")
                 data_service.sync_data()
                 stayed_offline = False
-                print("\n\nVocê está online novamente\n\n")
                 main_menu.show_options()
         elif not stayed_offline:
             file.log("network.log", "is Offline")
