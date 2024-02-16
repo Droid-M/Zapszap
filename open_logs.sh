254fa868471ec2f1b17fd7fcf6008a1dfbc7085c
Droid-M <62139366+Droid-M@users.noreply.github.com>
2024-02-16 12:19:02 -0300

    feat: create open_logs.sh

---
+++ open_logs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Verifica se o programa gedit está instalado
+if ! command -v gedit &> /dev/null; then
+    echo "O programa 'gedit' não está instalado."
+    exit 1
+fi
+
+# Caminho completo para a pasta logs
+log_path="$HOME/Documentos/GitHub/Zapszap/src/logs/"
+
+# Verifica se o diretório existe
+if [ ! -d "$log_path" ]; then
+    echo "O diretório de logs '$log_path' não existe."
+    exit 1
+fi
+
+# Lista todos os arquivos com a extensão .log no diretório especificado
+logs=$(find "$log_path" -type f -name "*.log")
+
+# Abre todos os arquivos .log encontrados no gedit como root, em uma única janela
+pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY gedit --new-window $logs
