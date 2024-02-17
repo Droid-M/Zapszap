28d333329eca1e28ed76e021b3c6f037d6acf72e
Droid-M <62139366+Droid-M@users.noreply.github.com>
2024-02-17 17:11:59 -0300

    update methods.py

--- src/globals/methods.py
+++ src/globals/methods.py
@@ -1,7 +1,8 @@
 from helpers import file, client
+import json
 
 _private_key = None
-_last_answer_host = {}
+ANSWERS_BACKUP_FILE = "answers.zap"
 
 def get_private_key():
     global _private_key
@@ -9,16 +10,22 @@ def get_private_key():
         _private_key = client.serialize_key(file.read_backup_file("private_key.zap"), True)
     return _private_key
 
+def get_last_answers():
+    _last_answer_host = file.read_backup_file(ANSWERS_BACKUP_FILE)
+    return _last_answer_host if _last_answer_host is not None else {}
+
 def get_last_answer_host(timestamp: str):
-    return _last_answer_host.get(timestamp)
+    return get_last_answers.get(timestamp)
 
 def set_last_answer_host(host: str, timestamp: str):
     file.log("answer.log", f"Nova resposta de host: {host} | time: {timestamp}")
-    global _last_answer_host
+    _last_answer_host = get_last_answers()
     _last_answer_host[timestamp] = host
+    file.write_backup_file(ANSWERS_BACKUP_FILE, json.dumps(_last_answer_host))
     
 def remove_last_answer_host(timestamp: str):
-    global _last_answer_host
+    _last_answer_host = get_last_answers()
     host = _last_answer_host.pop(timestamp, None)
     file.log("answer.log", f"Removendo host: {host} | time: {timestamp}")
+    file.write_backup_file(ANSWERS_BACKUP_FILE, json.dumps(_last_answer_host))
     return host
