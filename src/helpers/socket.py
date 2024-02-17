096dabbd39d16957a61dbab9652cdfc66eb5bd44
Droid-M <62139366+Droid-M@users.noreply.github.com>
2024-02-17 12:24:14 -0300

    update socket.py

--- src/helpers/socket.py
+++ src/helpers/socket.py
@@ -35,11 +35,11 @@ def send_message_to_partner(partner: Partner, message, is_json = True):
         
         while timeout > 0:
             time.sleep(DEFAULT_TIMEOUT / 5)
-            file.log("socket.log", f"partner-host: {partner.host}, answer-host: {get_last_answer_host()}")
             if get_last_answer_host() == partner.host:
                 successful = True
                 break
             timeout -= 1
+        file.log("socket.log", f"partner-host: {partner.host}, answer-host: {get_last_answer_host()}")
         
         client.disconnect_client(partner.socket)
         set_last_answer_host(None)
