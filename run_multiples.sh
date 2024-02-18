f7c6507099af02079dcd23bf7d8a5ebbc9b8e40f
Droid-M <62139366+Droid-M@users.noreply.github.com>
2024-02-18 14:55:07 -0300

    update run_multiples.sh

--- run_multiples.sh
+++ run_multiples.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-x-terminal-emulator -e "./d_build.sh 1"
-x-terminal-emulator -e "./d_build.sh 2"
-x-terminal-emulator -e "./d_build.sh 3"
+# x-terminal-emulator -e "./d_build.sh 1"
+x-terminal-emulator -e "./d_build.sh 2" &
+x-terminal-emulator -e "./d_build.sh 3" &
+x-terminal-emulator -e "./d_build.sh 4" &
+x-terminal-emulator -e "./d_build.sh 5" &
+x-terminal-emulator -e "./d_build.sh 6" &
