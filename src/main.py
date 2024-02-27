d495dc37f62768a8d6455cbdfd32f6035b2bca64
Droid-M <62139366+Droid-M@users.noreply.github.com>
2024-02-27 10:39:58 -0300

    update 2 files and create 1 file

--- src/main.py
+++ src/main.py
@@ -5,17 +5,20 @@ import threading
 from services import data_service as data_svc
 from multiprocessing import Process, Manager
 from globals import variables
+import sys
 
 CLIENTS = {}
 HOST = "localhost"
 PORT = int(file.env("SOCKET_PORT", 8050))
 
 if __name__ == '__main__':
+    if (len(sys.argv) < 3):
+        exit("\nQuantidade de argumentos inválidos! O mínimo requerido são 2 (main.py {user} {command} {optional:content}) conforme o exemplo: main.py Marcos read-messages")
+    
     # Restaura as mensagens e contatos anteriores
     data_svc.restore_data()
 
     # Sincroniza os dados com outros parceiros
-    print("Sincronizando dados, por favor, aguarde...")
     data_svc.sync_data()
 
     server_thread = threading.Thread(target=server.start)
@@ -33,6 +36,11 @@ if __name__ == '__main__':
     messages_terminal = Process(target=message_service.render_messages, args=(variables.INTERPROC_MESSAGES,))
     messages_terminal.start()
 
-    main_menu.show()
+    data_svc.set_username(sys.argv[1])
+    content = None
+    if (len(sys.argv) > 3):
+        content = sys.argv[3]
+    main_menu.run(sys.argv[2], content)
+    
     # Permite que subthreads parem:
     server.stop()
--- src/main_menu.py
+++ src/main_menu.py
@@ -1,43 +1,24 @@
 from helpers import input as InputHelper, menu, client, file
 from services import partner_service, message_service
-import traceback
 
-exit_menu = False
-
-def read_selection():
-    global exit_menu
-    ipt = InputHelper.choice("Sua escolha: ", InputHelper.input_integer, [1, 2, 3, 4, 5, 6])
-    menu.scroll_console()
-    if ipt == 1:
-        host = InputHelper.input_ip("Informe o IP do seu colega: ")
-        partner_service.start_partner_connection(host)
-    elif ipt == 2:
+def run(command: str, content = None):
+    command = command.lower()
+    if command == 'connect':
+        if (not content):
+            exit('Ip do parceiro é obrigatório!')
+        partner_service.start_partner_connection(content)
+    elif command == 'read-messages':
         message_service.see_chat()
-    elif ipt == 3:
+    elif command == 'list-partners':
         partner_service.list_partners()
-    elif ipt == 4:
+    elif command == 'exit-group':
         partner_service.exit_group()
-    elif ipt == 6:
+    elif command == 'restart':
         menu.restart()
+    elif command == 'send-message':
+        if (not content):
+            exit('Conteúdo da mensagem é obrigatório!')
+        message_service.send_group_message(content)
     else:
-        exit_menu = menu.close(False)
-
-def show_options():
-    print("Zapszap - Versão 1.0\n\n")
-    print(f"Endereço IP da máquina atual: {client.get_local_ip()}")
-    print("\nIndique a opção que deseja selecionar")
-    print("1 - Conectar-se a um colega\n2 - Visualizar chat\n3 - Visualizar lista de colegas\n4 - Sair do grupo\n5 - Sair da aplicação\n6 - Reiniciar aplicação")
-
-def show():
-    global exit_menu
-    menu.scroll_console()
-    while not exit_menu:
-        try:
-            show_options()
-            read_selection()
-        except Exception as e:
-            print(f"Ops, algum erro aconteceu :(. Mensagem: {e}")
-            file.log("error.log", traceback.format_exc())
-            menu.scroll_console(7)
-
-    
+        print("Comando inválido!")
+        # exit_menu = menu.close(False)
