de23036c2c2907318dabd8e870fabba8ed07e7c1
Droid-M <62139366+Droid-M@users.noreply.github.com>
2024-02-27 12:37:01 -0300

    update main_menu.py

--- src/main_menu.py
+++ src/main_menu.py
@@ -5,7 +5,7 @@ def run(command: str, content = None):
     command = command.lower()
     if command == 'connect':
         if (not content):
-            exit('Ip do parceiro é obrigatório!')
+            exit('IP do parceiro é obrigatório!')
         partner_service.start_partner_connection(content)
     elif command == 'read-messages':
         message_service.see_chat()
